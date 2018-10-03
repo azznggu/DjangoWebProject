de1a7370a459293f0aa898326cdb3b2188dc3407
azznggu <azznggu@gmail.com>
2018-10-03 14:38:28 +0900

    edit view.py for practice

--- DjangoWebProject/HelloDjangoApp/views.py
+++ DjangoWebProject/HelloDjangoApp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
 
 # Create your views here.
 def index(request) :
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+    
+    html_content = '<html><head><title>hello django test</title></head><body>'
+    html_content += '<strong>datetime</strong> on ' + now.strftime('%A, %d, %B, %Y at %X')
+    html_content += '</body></html>'
+
+    return HttpResponse(html_content)
 
 def home(request) :
     return HttpResponse("Hello, home!")
